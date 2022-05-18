88424380afd1f420abd717fcb9d4d9346c92b608
josefisto35 <103529204+josefisto35@users.noreply.github.com>
2022-05-18 14:58:28 +0700

    Predivine Variabel

--- php_dasar.php
+++ php_dasar.php
@@ -5,16 +5,9 @@
  <title>PHP Dasar</title>
 </head>
 <body>
- <h1>Belajar PHP Dasar</h1>
- <?php
- echo "Hello World";
- ?>
- <h1>Menggunakan Variabel</h1>
- <?php
- $nim = "0411500400";
- $nama = 'Abdullah';
- echo "NIM : " . $nim . "<br>";
- echo "Nama : $nama";
- ?>
+ <h1>Predivine Variabel</h1>
+<?php
+echo 'Selamat Datang ' . $_GET['nama'];
+?>
 </body>
 </html>
