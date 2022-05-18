389ad8226e8901d0b377da18ead7b125979f6101
josefisto35 <103529204+josefisto35@users.noreply.github.com>
2022-05-18 14:59:48 +0700

    Form Input

--- php_dasar.php
+++ php_dasar.php
@@ -5,9 +5,14 @@
  <title>PHP Dasar</title>
 </head>
 <body>
- <h1>Predivine Variabel</h1>
+<h2>Form Input</h2>
+<form method="post">
+ <label>Nama: </label>
+ <input type="text" name="nama">
+ <input type="submit" value="Kirim">
+</form>
 <?php
-echo 'Selamat Datang ' . $_GET['nama'];
+echo 'Selamat Datang ' . $_POST['nama'];
 ?>
 </body>
 </html>
